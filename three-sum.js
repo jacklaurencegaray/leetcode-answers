7d1f35b32cfa536960026215926c4a2f9e164260
jacklaurencegaray <jacklaurencegaray@users.noreply.github.com>
2022-08-06 14:25:13 +0200

    Added three sum

---
+++ three-sum.js
@@ -0,0 +1,61 @@
+/**
+ * Title: Three Sum
+ *
+ * Difficulty: medium
+ *
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ * Explanation:
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ * The distinct triplets are [-1,0,1] and [-1,-1,2].
+ * Notice that the order of the output and the order of the triplets does not matter.
+ * Example 2:
+ *
+ * Input: nums = [0,1,1]
+ * Output: []
+ * Explanation: The only possible triplet does not sum up to 0.
+ * Example 3:
+ *
+ * Input: nums = [0,0,0]
+ * Output: [[0,0,0]]
+ * Explanation: The only possible triplet sums up to 0.
+ *
+ * Constraints:
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ *
+ *
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+ const sortedNums = nums.sort((a, b) => a - b)
+ const results = []
+
+ for (let i = 0; i < sortedNums.length; i++) {
+  if (i != 0 && nums[i] === nums[i - 1]) continue
+
+  for (let left = i + 1, right = sortedNums.length - 1; left < right; ) {
+   const currentSum = sortedNums[i] + sortedNums[left] + sortedNums[right]
+   if (currentSum === 0) {
+    results.push([sortedNums[i], sortedNums[left], sortedNums[right]])
+    left++
+    while (left < right && sortedNums[left] === sortedNums[left - 1]) left++
+   } else if (currentSum > 0) {
+    right--
+   } else if (currentSum < 0) {
+    left++
+   }
+  }
+ }
+
+ return results
+}
