0193d02c6f8e80d92f79c3d11ae8b31ef6845305
jacklaurencegaray <jacklaurencegaray@users.noreply.github.com>
2022-08-31 17:15:17 +0200

    Added is subsequence problem

---
+++ is-subsequence.js
@@ -0,0 +1,44 @@
+/**
+ * Title: Is Subsequence
+ *
+ * Difficulty: easy
+ *
+ * Given two strings s and t,
+ * return true if s is a subsequence of t,
+ * or false otherwise.
+ *
+ * A subsequence of a string is a new string that is formed from the original
+ * string by deleting some (can be none) of the characters without
+ * disturbing the relative positions of the remaining characters.
+ * (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ *
+ * Example 1:
+ * Input: s = "abc", t = "ahbgdc"
+ * Output: true
+ *
+ * Example 2:
+ * Input: s = "axc", t = "ahbgdc"
+ * Output: false
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+var isSubsequence = function (s, t) {
+ if (s.length > t.length) return false
+
+ let i = 0,
+  j = 0
+
+ while (i < s.length && j < t.length) {
+  if (s.charAt(i) !== t.charAt(j)) {
+   j++
+  } else {
+   i++
+   j++
+  }
+ }
+
+ return i === s.length
+}
