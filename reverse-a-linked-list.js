e4a2f66548cba4672d8abe10aa55bfc8cc7e60e6
Jack <jacklaurencegaray@users.noreply.github.com>
2022-09-07 15:40:37 +0200

    Added reverse a linkedlist problem

---
+++ reverse-a-linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Title: Reverse a linkedlist
+ *
+ * Difficulty: easy
+ *
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * Example 1:
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ *
+ * Example 2:
+ * Input: head = [1,2]
+ * Output: [2,1]
+ *
+ * Example 3:
+ * Input: head = []
+ * Output: []
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (!head || !head.next) return head;
+
+  const newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return newHead;
+};
