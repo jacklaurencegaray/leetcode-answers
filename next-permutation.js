2a53a4ccef8c110ceea836b15a900f42f98ea171
jacklaurencegaray <jacklaurencegaray@users.noreply.github.com>
2022-10-10 17:03:09 +0200

    Added nextPermutation

---
+++ next-permutation.js
@@ -0,0 +1,62 @@
+/**
+ * Title: Next Permutation
+ *
+ * Difficulty: medium
+ *
+ * A permutation of an array of integers
+ * is an arrangement of its members into a sequence or linear order.
+ *
+ * For example, for arr = [1,2,3],
+ * the following are all the permutations of arr:
+ * [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+ * The next permutation of an array of integers
+ * is the next lexicographically greater permutation
+ * of its integer. More formally, if all the permutations of
+ * the array are sorted in one container according to their
+ * lexicographical order, then the next permutation of that
+ * array is the permutation that follows it in the sorted container.
+ * If such arrangement is not possible, the array must be
+ * rearranged as the lowest possible order (i.e., sorted in ascending order)
+ *
+ * For example, the next permutation of arr = [1,2,3] is [1,3,2].
+ * Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+ * While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+ * Given an array of integers nums, find the next permutation of nums.
+ *
+ * The replacement must be in place and use only constant extra memory.
+ *
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function nextPermutation(nums) {
+ let pivot, successor, i
+
+ const swap = (a, b) => {
+  const temp = nums[a]
+  nums[a] = nums[b]
+  nums[b] = temp
+ }
+
+ for (i = nums.length - 1; i > 0; i--) {
+  if (nums[i] > nums[i - 1]) {
+   pivot = i - 1
+   break
+  }
+ }
+
+ if (i === 0) pivot = -1
+
+ if (pivot !== -1) {
+  for (i = pivot + 1; i < nums.length; i++) {
+   if (nums[i] > nums[pivot]) successor = i
+  }
+
+  swap(pivot, successor)
+ }
+
+ for (i = 0; pivot + 1 + i <= nums.length - 1 - i; i++) {
+  swap(pivot + 1 + i, nums.length - 1 - i)
+ }
+
+ return nums
+}
