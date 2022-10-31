95eb647db2a785095f7fada55803dca05856594f
jacklaurencegaray <jacklaurencegaray@users.noreply.github.com>
2022-10-31 23:11:52 +0100

    Added jump game ii

---
+++ jump-game-ii.js
@@ -0,0 +1,51 @@
+/**
+ * Title: Jump Game II
+ *
+ * Difficulty: medium
+ *
+ * You are given a 0-indexed array of integers nums of length n.
+ * You are initially positioned at nums[0].
+ *
+ * Each element nums[i] represents the maximum length of a forward jump from index i.
+ * In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+ *
+ * 0 <= j <= nums[i] and
+ * i + j < n
+ *
+ * Return the minimum number of jumps to reach nums[n - 1].
+ * The test cases are generated such that you can reach nums[n - 1].
+ *
+ * Example 1:
+ * Input: nums = [2,3,1,1,4]
+ * Output: 2
+ * Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ *
+ * Example 2:
+ * Input: nums = [2,3,0,1,4]
+ * Output: 2
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+ let i = 0,
+  moves = 1,
+  jumps = 0,
+  maxReach = 0
+
+ for (; i < nums.length - 1 && maxReach < nums.length; i++) {
+  if (i > maxReach) return false
+
+  maxReach = Math.max(maxReach, i + nums[i])
+  moves--
+
+  if (!moves) {
+   moves = maxReach - i
+   jumps++
+  }
+ }
+
+ if (i + moves < nums.length) jumps++
+
+ return jumps
+}
